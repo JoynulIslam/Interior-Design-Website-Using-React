2ab607c47696191af142353c635280186ae669a2
JoynulIslam <joynul8763@gmail.com>
2026-02-18 17:40:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-blue-500">
+        <h1 className="text-4xl font-bold text-white">
+          React + Tailwind CSS 4
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
